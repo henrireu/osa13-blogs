b1ab2e052b0e9d09a8cbc4fce1fe96bebe52b319
reijomo <95440863+reijomo@users.noreply.github.com>
2025-01-09 14:48:17 +0200

    välipushaus

---
+++ controllers/users.js
@@ -0,0 +1,43 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch(error) {
+    next(error)
+  }
+})
+
+router.put('/:username', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { username: req.params.username } })
+    if (!user) {
+      const error = new Error('User not found')
+      error.status = 404
+      throw error
+    }
+
+    if (!req.body.name) {
+      const error = new Error('New name not found')
+      error.status = 404
+      throw error
+    }
+
+    user.name = req.body.name
+    await user.save()
+    res.json(req.body)
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
